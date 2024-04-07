f9503ac32415a1eac0e466c88f7cac55d8722b95
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-04-07 07:19:10 +0000

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/index'
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :lists
+  # post 'lists' => 'lists#create'
+  # get 'lists' => 'lists#index'
+  # get 'lists/index'
+  # # /lists/:id は /lists/1 や /lists/3 に該当する
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'lists/:id' => 'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
